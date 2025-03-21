22f28cee565aff3ad431aba8cf65d389b3ae0e22
Aarthi3730 <aarthikodipalli@gmail.com>
2025-03-21 23:35:54 +0530

    Addd rock paper scissors program

---
+++ RockPaperScisscors-PyProgram.py
@@ -0,0 +1,31 @@
+import random
+
+emojis = {'r': '🪨', 's': '✂️', 'p': '📃'}
+choices = ('r', 's', 'p')
+
+while True:
+    user_choice = input('Rock, Paper, or Scissors? (r/p/s): ').lower()
+    
+    if user_choice not in choices:
+        print("Invalid choice")
+        continue
+
+    computer_choice = random.choice(choices)
+    
+    print(f'Your choice: {emojis[user_choice]}')
+    print(f'Computer choice: {emojis[computer_choice]}')
+
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif (
+        (user_choice == 'r' and computer_choice == 's') or 
+        (user_choice == 's' and computer_choice == 'p') or 
+        (user_choice == 'p' and computer_choice == 'r')
+    ):
+        print('You win!')
+    else:
+        print('You lose!')
+
+    should_continue = input('Play again? (y/n): ').lower()
+    if should_continue == 'n':
+        break
